97e9a9e3b4d03f21102dcb28d9a0a87ebb453d20
Mheyrie <omota4fidelity@gmail.com>
2024-03-31 01:48:53 +0100

    Second section completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       colors: {
         brightRed: 'hsl(12, 88%, 59%)',
+        brightRedSupLight: 'hsl(15,100%,96.1%)',
         darkBlue: 'hsl(228, 39%, 23%)',
         darkGrayishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
