0fa3b443e22353c12c0ff5d1462884d4696dbf28
Mheyrie <omota4fidelity@gmail.com>
2024-04-02 22:37:15 +0100

    Initiated Carousel Js logic

--- Js/index.js
+++ Js/index.js
@@ -5,3 +5,68 @@ btn.addEventListener('click', () => {
     btn.classList.toggle('open')
     nav.classList.toggle('hidden')
 })
+
+const firstCircle = document.getElementById('carousel-btn')
+const secondCircle = document.getElementById('carousel-btnn')
+const threeCircle = document.getElementById('carousel-btnnn')
+const fourCircle = document.getElementById('carousel-btnnnn')
+
+const firstTes = document.getElementById('testimonal-one')
+const twoTes = document.getElementById('testimonal-two')
+const threeTes = document.getElementById('testimonal-three')
+const fourTes = document.getElementById('testimonal-four')
+
+
+
+const testimonal = [firstTes, twoTes, threeTes, fourTes]
+
+firstCircle.addEventListener('click', () => {
+    testimonal.forEach((testimony)=>{
+        if (testimony===firstTes){
+            testimony.classList.remove("hidden")
+            twoTes.classList.add('hidden')
+            threeTes.classList.add('hidden')
+        }
+        
+    })
+})
+// console.log(testimonal)
+
+secondCircle.addEventListener('click', () => {
+    testimonal.forEach((testimony)=>{
+
+        if (testimony===twoTes){
+            testimony.classList.remove("hidden")
+            firstTes.classList.add('hidden')
+            threeTes.classList.add('hidden')
+            fourTes.classList.add('hidden')
+
+        }
+    })
+})
+
+threeCircle.addEventListener('click', () => {
+    testimonal.forEach((testimony)=>{
+        if (testimony===threeTes){
+            testimony.classList.remove("hidden")
+            firstTes.classList.add('hidden')
+            twoTes.classList.add('hidden')
+            fourTes.classList.add('hidden')
+        }
+    })
+})
+
+fourCircle.addEventListener('click', () => {
+    testimonal.forEach((testimony)=>{
+        if (testimony===fourTes){
+            testimony.classList.remove("hidden")
+            firstTes.classList.add('hidden')
+            twoTes.classList.add('hidden')
+            threeTes.classList.add('hidden')
+        }
+    })
+})
+
+// if (testimony!==twoTes){
+//     if (testimony.classList.includes("hidden")===false) {testimony.classList.add("hidden")}
+// }
